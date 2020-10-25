71220c124c7b804128bf5cea57ab5faf62a19672
Shivansh2287 <54775060+Shivansh2287@users.noreply.github.com>
2020-10-26 03:04:24 +0530

    signinform

--- src/components/Signin/SigninElements.js
+++ src/components/Signin/SigninElements.js
@@ -11,11 +11,7 @@ top:0;
 right:0;
 z-index:0;
 overflow:hidden;
-background:linear-gradient(
-    108deg,
-    rgba(1,147,86,1)0%,
-    rgba(10,201,122,1)100%,
-);
+background:lightgreen;
 `
 
 export const FormWrap = styled.div`
@@ -25,7 +21,7 @@ flex-direction:column;
 justify-content:center;
 
 @media screen and (max-width:400px){
-    height:80%auto;
+    height:80%;
 }
 `
 export const Icon = styled(Link)`
@@ -42,7 +38,7 @@ font-size:32px;
 }
 `
 export const FormContent = styled.div`
-height:100%auto;
+height:100%;
 display:flex;
 flex-direction:column;
 justify-content:center;
@@ -55,7 +51,7 @@ export const Form = styled.form`
 background:#010101;
 max-width:400px;
 height:auto;
-width:100%auto;
+width:100%;
 z-index:1;
 display:grid;
 margin:0 auto;
@@ -67,33 +63,34 @@ box-shadow:0 1px 3px rgba(0,0,0,0.9);
     padding:32px 32px;
 }
 `
-export const FormH1 = styled.div`
+export const FormH1 = styled.h1`
 margin-bottom:40px;
 color:#fff;
 font-size:20px;
 font-weight:400;
 text-align:center;
 `
-export const FormLable = styled.div`
+export const FormLable = styled.label`
 margin-bottom:8px;
 font-size:14px;
 color:#fff;
 `
-export const FormInput = styled.div`
+export const FormInput = styled.input`
 padding:16px 16px;
 margin-bottom:32px;
 border:none;
 border-radius:4px;
 `
-export const FormButton = styled.div`
+export const FormButton = styled.button`
 background:#01bf71;
 padding:16px 0;
 border:none;
 border-radius:4px;
 color:#fff;
+font-size:20px;
 cursor: pointer;
 `
-export const Text = styled.div`
+export const Text = styled.span`
 text-align:center;
 margin-top:24px;
 color:#fff;
